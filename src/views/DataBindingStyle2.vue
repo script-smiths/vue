6c6c73be5e51e4556990a25c7833179de2eb8df0
csw <susu199037@gmail.com>
2025-04-07 18:32:00 +0900

    feat: data binding

---
+++ src/views/DataBindingStyle2.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :style="[baseStyle, emphasisStyle]">
+    스타일 배열 바인딩 예제입니다!
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      baseStyle: {
+        color: 'white',
+        backgroundColor: 'steelblue',
+        padding: '10px'
+      },
+      emphasisStyle: {
+        fontWeight: 'bold',
+        fontSize: '20px'
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/DataBindingTextarea.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <textarea v-model="content" placeholder="내용을 입력하세요" rows="5" cols="30"></textarea>
+    <p>입력한 내용:</p>
+    <pre>{{ content }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: ''
+    }
+  }
+}
+</script>
