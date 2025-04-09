3a270334d998046398446ef628fd14ba0f204ce9
csw <susu199037@gmail.com>
2025-04-10 01:57:25 +0900

    feat: computed and watch

---
+++ src/views/ComputedLifeCycleHook.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>{{ fullName }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: '홍',
+      lastName: '길동'
+    }
+  },
+  computed: {
+    fullName() {
+      console.log('computed fullName 실행');
+      return `${this.firstName} ${this.lastName}`;
+    }
+  },
+  mounted() {
+    console.log('mounted');
+  }
+}
+</script>
---
+++ src/views/WatchExample2.vue
@@ -0,0 +1,20 @@
+<template>
+  <input v-model="age" type="number" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      age: 18
+    }
+  },
+  watch: {
+    age(newValue) {
+      if (newValue >= 19) {
+        console.log('성인입니다!');
+      }
+    }
+  }
+}
+</script>
