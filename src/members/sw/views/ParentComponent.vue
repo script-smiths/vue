4b4b6c0805f5ee5dc26714c7205e0f1144ee5829
csw <susu199037@gmail.com>
2025-04-22 14:21:37 +0900

    feat: component

---
+++ src/members/sw/views/ParentComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <ChildComponent ref="child_component" />
+</template>
+<script>
+import ChildComponent from "@/members/sw/views/ChildComponent.vue";
+
+export default {
+  components: {ChildComponent},
+  mounted() {
+    this.$refs.child_component.$refs.btn.click();
+  }
+}
+</script>
---
+++ src/members/sw/views/ParentComponent5.vue
@@ -0,0 +1,21 @@
+<template>
+  <button type="button" @click="checkChild">자식 컴포넌트 데이터 조회</button>
+  <child-component5 ref="child_component" />
+</template>
+<script>
+import ChildComponent5 from "@/members/sw/views/ChildComponent5.vue";
+
+export default {
+  components: {ChildComponent5},
+  computed: {
+    msg() {
+      return this.$refs.child_component.msg;
+    },
+  },
+  methods: {
+    checkChild() {
+      alert(this.msg);
+    },
+  }
+}
+</script>
